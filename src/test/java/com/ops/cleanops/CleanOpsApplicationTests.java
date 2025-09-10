ddc4f93bccc76cd504ef3d88389684354e5e7c80
Cleaner <1414984960@qq.com>
2025-09-10 08:54:51 +0800

    feat: init project

---
+++ src/test/java/com/ops/cleanops/CleanOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ops.cleanops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
